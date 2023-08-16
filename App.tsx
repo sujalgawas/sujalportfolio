bdde50aa478fcccadab40df061907c6c0d1cb811
sujal gawas <sujalgawas18@gmail.com>
2023-08-16 13:32:08 +0530

    project setup

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.rtl.min.css";
+
+function App() {
+  return (
+    <h2>Portfolio project...</h2>
+  );
+}
+
+export default App;
